dfa92eaae499b19dc729c7916cf67bd13ac5848f
pmarkette <paul@markette.net>
2016-10-20 22:45:39 -0700

    Fix all the references so that site builds correctly with Gulp. Added my Google Maps API key.

--- README.md
+++
@@ -1,3 +0,0 @@
-http://pmarkette.github.io/frontend-nanodegree-resume/
-
-For this project I wrote all the data in JSON, then used javascript and jQuery to dynamically build the DOM.
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -163,7 +163,7 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+gulp.task('build', [/*'lint',*/ 'html', 'images', 'fonts', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
